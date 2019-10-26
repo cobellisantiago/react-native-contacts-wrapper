cbf4871882ae6dae8ea09eb53d7f40a1bc6612ee
马涛 <iwater@gmail.com>
2019-10-26 15:25:35 +0800

    change sdk compileSdkVersion

--- ios/RCTContactsWrapper/RCTContactsWrapper.h
+++ ios/RCTContactsWrapper/RCTContactsWrapper.h
@@ -6,7 +6,11 @@
 //  Copyright © 2016 Facebook. All rights reserved.
 //
 
-#import "RCTBridgeModule.h"
+#if __has_include(<React/RCTBridgeModule.h>)
+    #import <React/RCTBridgeModule.h>
+#else
+    #import "RCTBridgeModule.h"
+#endif
 
 @import Contacts;
 @import ContactsUI;
